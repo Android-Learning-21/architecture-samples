e3cf828fb2b25401f9f384b08531f92500629b62
Mario-paul <15961362+Mario-paul@users.noreply.github.com>
2023-08-15 15:56:52 -0400

    codelab step 5: Test the local data source
    - create TaskDaoTest test class
    - add a test database
    - add a test
    - confirm that test works

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/data/source/local/TaskDaoTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/data/source/local/TaskDaoTest.kt
@@ -15,3 +15,52 @@
  */
 
 package com.example.android.architecture.blueprints.todoapp.data.source.local
+
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+// Create test class
+class TaskDaoTest {
+    private lateinit var database: ToDoDatabase
+
+    @Before
+    fun initDb() {
+        // Add a test database
+        // create database in memory instead of storage, which is much faster since it doesn't persist
+        database = Room.inMemoryDatabaseBuilder(
+            getApplicationContext(),
+            ToDoDatabase::class.java
+        ).allowMainThreadQueries().build()
+    }
+
+    // Add a test
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun insertTaskAndGetTasks() = runTest {
+
+        // Create task object
+        val task = LocalTask(
+            title = "title",
+            description = "description",
+            id = "id",
+            isCompleted = false,
+        )
+        // Upsert task into our test database
+        database.taskDao().upsert(task)
+
+        // Get first object in database
+        val tasks = database.taskDao().observeAll().first()
+
+        // Test that the two actions above worked successfully
+//        assertEquals(0, tasks.size) // purposefully failing test, to test the test :)
+        assertEquals(1, tasks.size) // checks that upsert() works (1 task in database)
+        assertEquals(task, tasks[0]) // checks that observeAll() works (task in database matches the one we inserted)
+
+    }
+}
